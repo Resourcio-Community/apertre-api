13137621695d3bf676f49b8fbf70fe943d9dd4f2
ABHIRUP <abhirupsaha02@gmail.com>
2024-01-21 00:50:23 +0530

    update

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
 import { connectDB } from './db/connect.js'
-import { prRoute } from './router/PR.route.js'
 import { mentorRoute } from './router/mentor.route.js'
 import { menteeRoute } from './router/mentee.route.js'
 import { repoRoute } from './router/repo.route.js'
@@ -23,7 +22,6 @@ app.use(morgan('tiny'))
 app.use('/api/v1/mentor', mentorRoute)
 app.use('/api/v1/mentee', menteeRoute)
 app.use('/api/v1/repo', repoRoute)
-app.use('/api/v1/pr', prRoute)
 
 
 const port = process.env.PORT || 3000
--- router/PR.route.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { fetchAllData } from '../controllers/PR.controller.js'
-
-const router = Router()
-
-router.route('/').get(fetchAllData)
-
-export { router as prRoute }
